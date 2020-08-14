1b783bb6ff69d464d38562e4c48f24b89b68d1bf
Kisan Tamang <kisan@cloudyfox.com>
2020-08-14 12:36:39 +0545

    cognito added

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-east-1_ZYEAHlPUz", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "218b4sq73jar34kqkde60qsonm", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl:
+      " arn:aws:cognito-idp:us-east-1:860269591288:userpool/us-east-1_ZYEAHlPUz", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
